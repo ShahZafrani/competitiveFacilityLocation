a5ce82e2b76b1c1b0a526485609a5c34816c8cc7
Shah Zafrani <szz786@gmail.com>
2017-08-15 19:34:59 -0400

    added functioning score and button disable. Then broke it

--- main.js
+++ main.js
@@ -2,27 +2,58 @@ var redScore = 0;
 var blueScore = 0;
 
 var whoseTurn = "Red Player";
-
+var gameSize = 11;
 var red = "Red Player";
 var blue = "Blue Player";
 
-function updateScore()
+var maxVal = 10;
+var minVal = 1;
+
+function updateView()
 {
   document.getElementById("redScore").innerHTML = redScore;
   document.getElementById("blueScore").innerHTML = blueScore;
+  document.getElementById("whoseTurn").innerHTML = "It is currently " + whoseTurn + "'s turn";
+}
+
+function changeTurn() {
+  if(whoseTurn == red){
+    whoseTurn = blue;
+  } else if (whoseTurn == blue){
+    whoseTurn = red;
+  }
 }
 
 function updateButton(button)
 {
   button.disabled=true;
   var weight = parseInt(button.innerHTML);
+  console.log(weight);
   if(whoseTurn == red) {
     button.className = "redClicked";
     redScore += weight;
-  }
-  else if (whoseTurn == blue) {
+  } else if (whoseTurn == blue) {
     button.className = "blueClicked";
     blueScore += weight;
   }
-  updateScore();
+  changeTurn();
+  updateView();
+}
+
+function generateGameBoard() {
+  var rnJesus = 10;
+  var container = document.getElementById("node-container");
+  
+  for (i = 0; i < gameSize; i++) {
+    rnJesus = Math.floor((Math.random() * maxVal) + minVal);
+    var nodeId = "node" + i;
+    var button = document.createElement('button');
+    button.className = "node-button";
+    button.innerHTML = rnJesus;
+    button.id = nodeId;
+    container.appendChild(button);
+    // button.onclick = updateButton;
+    document.getElementById(nodeId).onclick = updateButton(this);
+  }
+  updateView();
 }
